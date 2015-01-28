2be8aba08db1231082a8bf15f81f15564bbc05aa
Stefan4472 <Stefan4472@users.noreply.github.com>
2015-01-28 14:35:58 -0500

    Update Settings.java

--- Settings.java
+++ Settings.java
@@ -1,9 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 
 public class Settings {
     public static int red = 0;
@@ -11,6 +11,9 @@ public class Settings {
     public static int blue = 0;
     public static double variance = 0.0;
     public static String file_import = "Enter file name here";
+    //public static Trial[] loaded_trials = new Trial[20];
+    //Vector<String> myVector=new Vector<String>(10,2);
+    public Vector<Trial> loaded_trials = new Vector<Trial>(0,1);
     /// note: it is better practice to access these variables with "getter" and "setter" methods
     public static int GetRed() {return red;}
     public static void SetRed(int set) {red = set; UpdateSettings();};
@@ -21,10 +24,17 @@ public class Settings {
     public static double GetVariance() {return variance;}
     public static void SetVariance(double set) {variance = set;}
     public static String GetFileImport() {return file_import;}
-    public static void SetFileImport(String file_name) {
-        file_import = file_name;
-        UpdateSettings();
-    } 
+    public static void SetFileImport(String file_name) {file_import = file_name; UpdateSettings();}
+    //public static Trial[] GetTrials() {return loaded_trials;};
+    public Vector<Trial> GetTrials() {return loaded_trials;}
+    public void AddTrial(Trial add) { /* adds trial to vector of trials */
+        loaded_trials.addElement(add);
+        boolean success = Settings.UpdateTrials();
+    }
+    public void PrintTrials() {
+        for(int i = 0; i < loaded_trials.size(); i++)
+            Trial.Println(loaded_trials.get(i));
+    }
     public static boolean LoadSettings() {
         boolean load = true;
         try {
@@ -71,6 +81,44 @@ public class Settings {
             write_settings.newLine();
             write_settings.write(Integer.toString(blue));
             write_settings.close();
+        } catch(IOException e) {
+            //Print("Error writing to file\n");
+            success = false;
+        }
+        return success;
+    }
+    public boolean LoadTrials() {
+        boolean load = true;
+        try {
+            FileReader file = new FileReader("AppleAnalysis_Trials");
+            BufferedReader read_trials = new BufferedReader(file);
+            String line = "";
+            while((line = read_trials.readLine()) != null) {
+                boolean success[] = new boolean[1];
+                Trial load_trial = Trial.ReadDataFile(line, success); /* attempts to read
+                in trial data */
+                if(success[0]) {
+                    AddTrial(load_trial); /* add trial to array */
+                } 
+            }   
+        } catch (IOException e) {
+            //Print("Error reading settings file\n");
+            load = false;
+        }
+        return load; /* returns whether "TextManipulator_Settings" was accessed and read */
+    }
+    public boolean UpdateTrials() {
+        boolean success = true;
+        try {
+            FileWriter file = new FileWriter("AppleAnalysis_Trials");
+            BufferedWriter write_trials = new BufferedWriter(file);
+            /* write settings to file (in the correct order), first converting each value to a char and comma-separating them */
+            for(int i = 0; i < loaded_trials.size(); i++) {
+                Trial write_trial = loaded_trials.get(i);
+                write_trials.write(write_trial.GetFileName()); /* write file name of the trial to file */
+                write_trials.newLine();
+            }
+            write_trials.close();
         } catch(IOException e) {
             //Print("Error writing to file\n");
             success = false;
