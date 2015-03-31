8c61360cb2707fecfeb7d4568173ed85f2874d05
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-31 17:01:06 -0400

    Refactoring and Fixes

--- Settings.java
+++ Settings.java
@@ -10,56 +10,43 @@ public class Settings {
     public int green = 0;
     public int blue = 0;
     public double variance = 0.0;
+    public Settings(String file_name) {
+        loadSettings(file_name);
+    }
     public String file_import = "Enter file name here";
     public ArrayList<Trial> loaded_trials = new ArrayList<>();
-    /// note: it is better practice to access these variables with "getter" and "setter" methods
-    public int GetRed() {return red;}
-    public void SetRed(int set) {red = set; UpdateSettings();};
-    public int GetGreen() {return green;}
-    public void SetGreen(int set) {green = set; UpdateSettings();};
-    public int GetBlue() {return blue;}
-    public void SetBlue(int set) {blue = set; UpdateSettings();}
-    public double GetVariance() {return variance;}
-    public void SetVariance(double set) {variance = set;}
-    public String GetFileImport() {return file_import;}
-    public void SetFileImport(String file_name) {file_import = file_name; UpdateSettings();}
-    public ArrayList<Trial> GetTrials() {return loaded_trials;}
-    public void SetTrials(ArrayList<Trial> new_list) {loaded_trials = new_list;}
-    public void AddTrial(Trial add) { /* adds trial to vector of trials */
+    public int red() {return red;}
+    public void setRed(int set) {red = set; updateSettings("AppleAnalysis_Settings");};
+    public int green() {return green;}
+    public void setGreen(int set) {green = set; updateSettings("AppleAnalysis_Settings");};
+    public int blue() {return blue;}
+    public void setBlue(int set) {blue = set; updateSettings("AppleAnalysis_Settings");}
+    public double variance() {return variance;}
+    public void setVariance(double set) {variance = set;}
+    public String fileImport() {return file_import;}
+    public void setFileImport(String file_name) {file_import = file_name; updateSettings("AppleAnalysis_Settings");}
+    public ArrayList<Trial> trials() {return loaded_trials;}
+    public void setTrials(ArrayList<Trial> new_list) {loaded_trials = new_list;}
+    public void addTrial(Trial add) { /* adds trial to ArrayList of trials */
         loaded_trials.add(add);
-        UpdateTrials();
-    }
-    /* removes trial from array list */
-    public boolean RemoveTrial(Trial remove) {
-        boolean remove_success;
-        do {
-            remove_success = loaded_trials.remove(remove);
-        }while (remove_success);
-        return remove_success;
+        updateTrials("AppleAnalysis_Trials");
     }
     /* removes trial from array list using String trial_name */
-    public boolean RemoveTrial(String remove_name) {
+    public boolean removeTrial(String remove_name) {
         boolean remove_success = false;
         for(int i = loaded_trials.size() - 1; i > -1; i--) {
             /* if trial name matches name of trial to remove, remove it from list */
-            if(loaded_trials.get(i).GetTrialName().equals(remove_name)) {
-                System.out.println("Removed " + loaded_trials.get(i).GetTrialName());
+            if(loaded_trials.get(i).trialName().equals(remove_name)) {
                 loaded_trials.remove(i);
                 remove_success = true; /* specified trial found and removed successfully */
+                updateTrials("AppleAnalysis_Trials");
             }
         }
         return remove_success;
     }
-    public void PrintTrials() {
-        Trial trial = new Trial();
-        for(int i = 0; i < loaded_trials.size(); i++)
-            trial.Println(loaded_trials.get(i));
-        System.out.println(loaded_trials.size() + " trials loaded.");
-    }
-    public Settings LoadSettings() {
-        boolean load = true;
+    public boolean loadSettings(String file_name) {
         try {
-            FileReader file = new FileReader("AppleAnalysis_Settings");
+            FileReader file = new FileReader(file_name);
             BufferedReader read_settings = new BufferedReader(file);
             String line = "";
             int line_counter = 1;
@@ -81,22 +68,19 @@ public class Settings {
                 }
                 line_counter++;
             }
+            return true;
         } catch (IOException e) {
             //Print("Error reading settings file\n");
-            load = false;
+            return false;
+        } catch (NumberFormatException e) {
+            return false;
         }
-        Settings settings = new Settings();
-        settings.SetRed(red);
-        settings.SetGreen(green);
-        settings.SetBlue(blue);
-        settings.SetFileImport(file_import);
-        return settings; /* returns whether "TextManipulator_Settings" was accessed and read */
     }
     /* updates "TextManipulator_Settings" with new values from program - will be run every time the user changes settings */
-    public boolean UpdateSettings() {
+    public boolean updateSettings(String file_name) {
         boolean success = true;
         try {
-            FileWriter file = new FileWriter("AppleAnalysis_Settings");
+            FileWriter file = new FileWriter(file_name);
             BufferedWriter write_settings = new BufferedWriter(file);
             /* write settings to file (in the correct order), first converting each value to a char and comma-separating them */
             write_settings.write(file_import);
@@ -113,10 +97,10 @@ public class Settings {
         }
         return success;
     }
-    public boolean LoadTrials() {
+    public boolean loadTrials(String file_name) {
         boolean load = true;
         try {
-            FileReader file = new FileReader("AppleAnalysis_Trials");
+            FileReader file = new FileReader(file_name);
             BufferedReader read_trials = new BufferedReader(file);
             String line = "";
             while((line = read_trials.readLine()) != null) {
@@ -124,7 +108,7 @@ public class Settings {
                 Trial load_trial = new Trial();
                 load_trial = load_trial.ReadDataFile(line, success);
                 if(success[0]) {
-                    AddTrial(load_trial); /* add trial to array */
+                    addTrial(load_trial); /* add trial to array */
                 } 
             }   
         } catch (IOException e) {
@@ -133,24 +117,21 @@ public class Settings {
         }
         return load; /* returns whether "TextManipulator_Settings" was accessed and read */
     }
-    public boolean UpdateTrials() {
-        boolean success = true;
+    public boolean updateTrials(String file_name) {
         try {
-            FileWriter file = new FileWriter("AppleAnalysis_Trials");
+            FileWriter file = new FileWriter(file_name);
             BufferedWriter write_trials = new BufferedWriter(file);
             /* write settings to file (in the correct order), first converting each value to a char and comma-separating them */
             for(int i = 0; i < loaded_trials.size(); i++) {
                 Trial write_trial = loaded_trials.get(i);
-                System.out.println("Updated " + write_trial.GetTrialName());
-                write_trials.write(write_trial.GetFileName()); /* write file name of the trial to file */
+                write_trials.write(write_trial.fileName()); /* write file name of the trial to file */
                 write_trials.newLine();
             }
             write_trials.close();
+            return true;
         } catch(IOException e) {
-            //Print("Error writing to file\n");
-            success = false;
+            return false;
         }
-        return success;
     }
 
 }
